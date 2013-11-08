72a111a30f20597a8e4a7874f89103f43a7dff7c
Slemgrim <slemgrim@gmailcom>
2013-11-08 11:01:03 +0100

    die on missing database

--- src/php/functions/getDatabase.php
+++ src/php/functions/getDatabase.php
@@ -2,5 +2,8 @@
 
 function getDatabase($dbHost, $dbUser, $dbPass, $dbDemo){
     $db = mysqli_connect($dbHost, $dbUser, $dbPass, $dbDemo);
+    if(!$db){
+        die('no database');
+    }
     return $db;
 }
